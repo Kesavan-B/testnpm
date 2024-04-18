a7d0fc95dcd42e8f1bd741d6423403f73154c706
kesavan <kesavanui.b@gmail.com>
2024-04-18 16:57:44 +0530

    message

--- index.js
+++ index.js
@@ -11,17 +11,4 @@ const downloadExcel = (data, name) => {
 }
 
 
-
-export const downloadPDF = (data, name) => {
-    const url = window.URL.createObjectURL(new Blob([data]));
-    const link = document.createElement('a');
-    link.href = url;
-    var filename = name + '.pdf'
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-}
-export default downloadPDF;
-
 module.exports.downloadExcel = downloadExcel
-module.exports.downloadPDF = downloadPDF
